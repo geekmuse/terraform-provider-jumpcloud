ccbf24b6f51b20d1339d97f33989a991d5ec1621
Brad Campbell <b@bradleyscampbell.net>
2017-01-24 08:47:13 -0500

    feat: working example build based on docs

---
+++ provider.go
@@ -0,0 +1,12 @@
+
+package main
+
+import (
+    "github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+    return &schema.Provider {
+        ResourcesMap: map[string]*schema.Resource{},
+    }
+}
