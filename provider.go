3e2beb7a26aed64c2f246935e105f549b75cf885
Brad Campbell <b@bradleyscampbell.net>
2017-02-08 07:44:30 -0500

    feat: add system schema; implement read/import for systemUser

--- provider.go
+++ provider.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-    "strings"
     "github.com/hashicorp/terraform/helper/schema"
     "github.com/geekmuse/jcapi"
 )
@@ -66,21 +65,53 @@ func Provider() *schema.Provider {
                 Read:       ReadSystemUser,
                 Update:     UpdateSystemUser,
                 Delete:     DeleteSystemUser,
+                Importer:   &schema.ResourceImporter{
+                    State: ImportSystemUser,
+                },
             },
-            // "jumpcloud_system": &schema.Resource{
-            //     Schema: map[string]*schema.Schema{
-            //         "user_name": &schema.Schema{
-            //             Type:       schema.TypeString,
-            //             Required:   true,
-            //             ForceNew:   true,
-            //         },
-            //     },
-            //     SchemaVersion:  1,
-            //     Create:     CreateSystem,
-            //     Read:       ReadSystem,
-            //     Update:     UpdateSystem,
-            //     Delete:     DeleteSystem,
-            // },
+            "jumpcloud_system": &schema.Resource{
+                Schema: map[string]*schema.Schema{
+                    "display_name": &schema.Schema{
+                        Type:       schema.TypeString,
+                        Required:   true,
+                        ForceNew:   false,
+                    },
+                    "allow_ssh_password_auth": &schema.Schema{
+                        Type:       schema.TypeBool,
+                        Required:   true,
+                        ForceNew:   false,
+                    },
+                    "allow_ssh_root_login": &schema.Schema{
+                        Type:       schema.TypeBool,
+                        Required:   true,
+                        ForceNew:   false,
+                    },
+                    "allow_multifactor_auth": &schema.Schema{
+                        Type:       schema.TypeBool,
+                        Required:   true,
+                        ForceNew:   false,
+                    },
+                    "allow_public_key_auth": &schema.Schema{
+                        Type:       schema.TypeBool,
+                        Required:   true,
+                        ForceNew:   false,
+                    },
+                    "tags": &schema.Schema{
+                        Type:       schema.TypeList,
+                        Elem:       &schema.Schema{Type: schema.TypeString},
+                        Required:   false,
+                        Optional:   true,
+                    },
+                },
+                SchemaVersion:  1,
+                Create:     CreateSystem,
+                Read:       ReadSystem,
+                Update:     UpdateSystem,
+                Delete:     DeleteSystem,
+                // Importer:   &schema.ResourceImporter{
+                //     State: ImportSystem,
+                // },
+            },
         },
         Schema:         map[string]*schema.Schema{
             "api_key": &schema.Schema{
@@ -109,7 +140,7 @@ func CreateSystemUser(d *schema.ResourceData, meta interface{}) error {
         Sudo:               d.Get("sudo").(bool),
         PasswordlessSudo:   d.Get("passwordless_sudo").(bool),
         AllowPublicKey:     d.Get("allow_public_key").(bool),
-        PublicKey:          strings.Replace(d.Get("public_key").(string), "\n", "", -1),
+        PublicKey:          d.Get("public_key").(string),
         Activated:          true,
         ExternallyManaged:  false,
     }
@@ -124,7 +155,50 @@ func CreateSystemUser(d *schema.ResourceData, meta interface{}) error {
     return nil
 }
 
+// Adding systems in JumpCloud only allowed by Kickstart script.
+// Once a system has been created in that way, it can be imported
+// using Terraform's "import" command.
+func CreateSystem(d *schema.ResourceData, meta interface{}) error {
+
+    return nil
+}
+
+
 func ReadSystemUser(d *schema.ResourceData, meta interface{}) error {
+    jcUser, err := meta.(*jcapi.JCAPI).GetSystemUserById(d.Id(), true)
+
+    if err != nil {
+        return err
+    }
+
+    d.Set("user_name", jcUser.UserName)
+    d.Set("first_name", jcUser.FirstName)
+    d.Set("last_name", jcUser.LastName)
+    d.Set("email", jcUser.Email)
+    // Not implemented in getJCUserFieldsFromInterface
+    // d.Set("password", jcUser.Password)
+    d.Set("sudo", jcUser.Sudo)
+    d.Set("passwordless_sudo", jcUser.PasswordlessSudo)
+    // Not implemented in getJCUserFieldsFromInterface
+    // d.Set("allow_public_key", jcUser.AllowPublicKey)
+    d.Set("public_key", jcUser.PublicKey)
+    d.Set("uid", jcUser.Uid)
+    d.Set("gid", jcUser.Gid)
+    d.Set("enable_managed_uid", jcUser.EnableManagedUid)
+    d.Set("activated", jcUser.Activated)
+    d.Set("externally_managed", jcUser.ExternallyManaged)
+    return nil
+}
+
+func ImportSystemUser(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+    if err := ReadSystemUser(d, meta); err != nil {
+        return nil, err
+    }
+
+    return []*schema.ResourceData{d}, nil
+}
+
+func ReadSystem(d *schema.ResourceData, meta interface{}) error {
 
     return nil
 }
@@ -144,7 +218,7 @@ func UpdateSystemUser(d *schema.ResourceData, meta interface{}) error {
     jcUser.Sudo  =              d.Get("sudo").(bool)
     jcUser.PasswordlessSudo =   d.Get("passwordless_sudo").(bool)
     jcUser.AllowPublicKey =     d.Get("allow_public_key").(bool)
-    jcUser.PublicKey =          strings.Replace(d.Get("public_key").(string), "\n", "", -1)
+    jcUser.PublicKey =          d.Get("public_key").(string)
     jcUser.Activated =          true
     jcUser.ExternallyManaged =  false
 
@@ -157,6 +231,12 @@ func UpdateSystemUser(d *schema.ResourceData, meta interface{}) error {
     d.SetId(userId)
     return nil
 }
+
+func UpdateSystem(d *schema.ResourceData, meta interface{}) error {
+
+    return nil
+}
+
 func DeleteSystemUser(d *schema.ResourceData, meta interface{}) error {
     jcUser, err := meta.(*jcapi.JCAPI).GetSystemUserById(d.Id(), true)
 
@@ -171,6 +251,11 @@ func DeleteSystemUser(d *schema.ResourceData, meta interface{}) error {
     }
 
     d.SetId("")
+    return nil
+}
+
+func DeleteSystem(d *schema.ResourceData, meta interface{}) error {
+
     return nil
 }
 
